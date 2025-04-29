bee14e3f44bf5a9970ac55afb2f5d04caa56b347
Pankaj Agrawal <pankaj.ag19@gmail.com>
2025-04-30 01:24:09 +0530

    Added validations

--- app/Http/Controllers/AttendeeController.php
+++ app/Http/Controllers/AttendeeController.php
@@ -9,10 +9,18 @@ class AttendeeController extends Controller
 {
     public function store(Request $request)
     {
+        
+        $attendeeDetails = Attendee::where('email', $request->input('email'))->first();
+
+        if($attendeeDetails){
+            return response()->json(['message' => 'Email is already registered.'], 400); 
+        }
+
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:attendees,email',
         ]);
+
 
         $attendee = Attendee::create($validated);
 
--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -11,11 +11,23 @@ class BookingController extends Controller
 {
     public function store(Request $request)
     {
+        $attendee = Attendee::find($request->attendee_id);
+        if(!$attendee){
+            return response()->json(['message' => 'Attendee is not registered.'], 400);   
+        }
+        
+        $eventDetails = Event::find($request->event_id);
+        if(!$eventDetails){
+            return response()->json(['message' => 'Event is not registered.'], 400);   
+        }
+
         $validated = $request->validate([
             'event_id' => 'required|exists:events,id',
             'attendee_id' => 'required|exists:attendees,id',
         ]);
 
+        
+        
         $event = Event::findOrFail($validated['event_id']);
 
         if ($event->bookings()->count() >= $event->capacity) {
